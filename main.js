33ce1cd533ce3257c01796526b93399ae62eb1cd
Jasper <57488312+Jasper-27@users.noreply.github.com>
2021-10-24 22:11:16 +0100

    Update main.js
    removed some comments and shit, from an old project

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-/*
-To get this program to work you need two files
 
-1. token.txt (A file containing your discord bots token)
-
-
-*/
-
-//https://discord.com/api/oauth2/authorize?client_id= 758403936784482315&scope=bot&permissions=1
 
 const Discord = require('discord.js')
 const fs = require('fs');
@@ -16,10 +8,6 @@ const client = new Discord.Client()
 const { exec } = require("child_process");
 
 
-
-//https://discord.com/oauth2/authorize?client_id=717442260131774485&scope=bot
-
-
 var token = fs.readFileSync('token',{encoding:'utf8', flag:'r'});
 var theUserIndex = null
 token = token.replace(/(\r\n|\n|\r)/gm, ""); //Removes the newline from the token file
@@ -27,32 +15,22 @@ client.login(token)
 
 
 
-
-//Runs neofetch on the server. The program must be installed 
+// run a command s
 function runCommand(msg){
 
     var command = msg.content.substring(6);
 
     exec(command, (error, stdout, stderr) => {
-        // if (error) {
-        //     //console.log(`error: ${error.message}`);
-        //     msg.reply("Could not run neofetch")
-        //     return;
-        // }
         if (stderr) {
             msg.reply(`There was an error: ${stderr}`)
             console.log(`stderr: ${stderr}`);
             return;
         }
-        msg.reply(`\n${stdout}`);
+        console.log(`${stdout}`)
+        msg.channel.send(`\n${stdout}`)
     });
 
 }
-
-
-
-
-
 
 
 //When the client connects
@@ -60,15 +38,13 @@ client.on('ready', () => {
 
   console.log(`logged in as ${client.user.tag}!`)
 
+  console.log("details: ")
   console.log(client.user.id)
-
-  // Does this  work? Well at some point it did 
-
 
   //Sets the bots username and activity
   client.user.setUsername('DISH');
   //client.user.setActivity(':-)');
-  client.user.setActivity('your shit', { type: 'pwning' });
+  client.user.setActivity('YOUR MACHINES', { type: 'WATCHING' });
 
   
 
@@ -88,23 +64,14 @@ client.on('message', msg => {
 
   
     // Commands (Soon this will be a different thing )
-    if (msg.author.id === "326743504443146241" ){  // My ID.
-        var arg = null
-
-
+    if (msg.author.id === "326743504443146241" ){  // My ID. This stops people running commands, on my bot
 
         if(msg.content.substring(0,5) == "DISH:"){
 
-           
             runCommand(msg)
-
-
 
         }
     }
-  
-
-
 
 
 })
