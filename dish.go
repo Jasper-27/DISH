2b069eec51d17ccd0cd6d2bb78f55fa68cbdc97d
Jasper <57488312+Jasper-27@users.noreply.github.com>
2021-11-29 23:42:00 +0000

    Code cleanup

--- dish.go
+++ dish.go
@@ -53,40 +53,29 @@ func main() {
 }
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
-	} else {
-		fmt.Println(m)
 	}
 
 	if strings.Contains(strings.ToLower(m.Content), "!test") {
 		s.ChannelMessageSend(m.ChannelID, "testSuccesfull")
 	}
 
-	// run command on all nodes
-	// firstCharacter := m.Content[0:2]
-
+	// Running command if sent to all nodes
 	if strings.HasPrefix(m.Content, "! ") {
 		command_string := m.Content[2:len(m.Content)] // get everything after the '! '
 		fmt.Println(command_string)
 
 		out, errorMessage := runCommand(command_string)
 
-		fmt.Println(out)
-		fmt.Println(errorMessage)
-
 		if errorMessage != "" {
 			fmt.Println(errorMessage)
-
 			s.ChannelMessageSend(m.ChannelID, errorMessage)
-
 			return
 		}
-
 		s.ChannelMessageSend(m.ChannelID, string(out))
-
 	}
 
 	fmt.Println(m.Author.Username, ": ", m.Content)
@@ -98,12 +87,14 @@ func runCommand(command string) (outString string, errorMessage string) {
 	var shell string
 	errorMessage = ""
 
+	// Selecting which shell to use
 	if runtime.GOOS == "windows" {
 		shell = "powershell.exe"
 	} else {
 		shell = "sh"
 	}
 
+	// run command, and if it causes an error create an error
 	out, err := exec.Command(shell, "-c", command).Output()
 	if err != nil {
 		fmt.Println(err.Error())
